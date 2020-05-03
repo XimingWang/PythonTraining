5523c0b9fc360f7e8f9378b067684d384dc95411
XimingWang <xiwmtl@gmail.com>
2020-05-03 10:29:41 -0400

    update description 06 Learning Standard Library

---
+++ 06 Leaning the Python 3 Starndard Library/description.md
@@ -0,0 +1,13 @@
+# Instructor
+ * Kathryn Hodge
+
+# WAHT'S INCLUDED
+ * Exercise Files
+ * Certificates
+
+# Course details
+ * 2h10m
+ * Beginner + Intermediate
+ * Released: 2/15/2017
+
+
---
+++ 06 Leaning the Python 3 Starndard Library/exercise.py
@@ -0,0 +1,11 @@
+import math
+#from random import random
+import random
+
+print(math.radians(360)/math.pi)
+
+possiblePets = ["cat","dog","fish"]
+print(random.choice(possiblePets))
+print(random.sample())
+
+
